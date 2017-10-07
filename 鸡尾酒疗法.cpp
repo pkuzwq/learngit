db3520d3a61d10974d2afaa737afe1c0db9b2b74
郑炜强 <1700017749@pku.end.cn>
2017-10-07 11:48:00 +0800

    add jiweijiu.cpp

---
+++ 鸡尾酒疗法.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a[100],b[100],x,y;
+    int n,i;
+    cin>>n;
+    cin>>a[1]>>b[1];
+    x=b[1]/a[1];
+    for (i=2;i<=n;i++)
+    {
+        cin>>a[i]>>b[i];
+        y=b[i]/a[i];
+        if (y-x>0.05) cout<<"better"<<endl;
+        else if (x-y>0.05) cout<<"worse"<<endl;
+        else cout<<"same"<<endl;
+    }
+    return 0;
+}
